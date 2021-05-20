394a999acc88dfa5ff08d22aba31e0b44759855a
adrianplanetas <dw1acovid@gmail.com>
2021-05-20 19:58:29 +0100

    1primer

---
+++ tutu/src/tutu/Tutu.java
@@ -0,0 +1,15 @@
+package tutu;
+
+public class Tutu {
+	
+	public boolean visualizarSalida(boolean acertada, int i, String newParam) {
+		return acertada;
+		
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
